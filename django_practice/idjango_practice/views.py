5201d4f434345235bef00623cd0830b968207b89
“Jordi” <jbg8@alumnes.udl.cat>
2017-04-19 19:42:34 +0200

    no viewsgit add *

--- django_practice/idjango_practice/views.py
+++ django_practice/idjango_practice/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
 
-def mainpage(request): # objecte de tipus http request k em proporciona django
-    page = """
-    <html>
-    <body>
-    sobres, a PP application
-    </body>
-    </html>
-    """
-    return HttpResponse() # construeix resposta hhttp a apartir duna cadena
-                        # de text k ha de contenir el html
-# Create your views here.
+
