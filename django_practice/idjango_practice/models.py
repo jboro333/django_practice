df63d2e0a4e4b1c9380d66c8f09bd8d63f09578c
Alberto Sanmartin Martinez <albertosanmartinmartinez@MacBook-Pro-de-Alberto.local>
2017-04-19 19:54:42 +0200

    models actuallized

--- django_practice/idjango_practice/models.py
+++ django_practice/idjango_practice/models.py
@@ -1,28 +1,37 @@
-from __future__ import unicode_literals
 
+from __future__ import unicode_literals
 from django.db import models
 
 
 # Create your models here.
 class Artist(models.Model):
-    id_artist = models.IntegerField()
+    id_artist = models.IntegerField(balnk=False, null=False)
     name = models.TextField()
+    albums = models.ForeignKey(Album, null=False, blank=False)
+    url = models.URLField()
+    user = models.ForeignKey(User, default=1)
 
     def getArtistInfo(slef):
+        pass
+
+    def getAlbums(self):
         pass
 
 
 class Song(models.Model):
     id_song = models.IntegerField()
     name_song = models.TextField()
+    album = models.ForeignKey(Album, null=False)
+    url = models.URLField()
 
     def getSongInfo(slef):
         pass
 
 
 class Album(models.Model):
-    id_album = models.IntegerField()
+    id_album = models.IntegerField(blank=False, null=False)
     name_album = models.TextField()
+    url = models.URLField()
 
     def getAlbumInfo(slef):
         pass
