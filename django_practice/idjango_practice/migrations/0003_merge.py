52173c405ddef00dcb8893a54e1deafe38a9c886
Alberto Sanmartin Martinez <albertosanmartinmartinez@MacBook-Pro-de-Alberto.local>
2017-04-20 00:31:44 +0200

    more worked

---
+++ django_practice/idjango_practice/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('idjango_practice', '0002_auto_20170419_1728'),
+        ('idjango_practice', '0002_auto_20170419_1719'),
+    ]
+
+    operations = [
+    ]
--- django_practice/idjango_practice/models.py
+++ django_practice/idjango_practice/models.py
@@ -6,41 +6,61 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Artist(models.Model):
-    id_artist = models.IntegerField(balnk=False, null=False)
-    name = models.TextField()
-    albums = models.ForeignKey(Album, null=False, blank=False)
+    id_artist = models.IntegerField(blank=False, null=False)
+    artist_name = models.TextField()
+    # albums = models.ForeignKey(Album, null=False, blank=False)
     url = models.URLField()
-    user = models.ForeignKey(User, default=1)
+    user = models.ForeignKey(User)
 
-    def getArtistInfo(slef):
+    def getArtistInfo(self, id_artist):
+        return "Artist name: " + self.artist_name
+
+    def getArtistUrl(self):
         pass
 
-    def getAlbums(self):
+
+class Album(models.Model):
+    id_album = models.IntegerField()
+    album_name = models.TextField()
+    artist = models.ForeignKey(Artist)
+    url = models.URLField()
+
+    def getAlbumInfo(self):
+        return "Album name: " + self.album_name
+
+    def getAlbumUrl(self):
         pass
 
 
 class Song(models.Model):
     id_song = models.IntegerField()
-    name_song = models.TextField()
-    album = models.ForeignKey(Album, null=False)
+    song_name = models.TextField()
+    album = models.ForeignKey(Album)
+    artist = models.ForeignKey(Artist)
     url = models.URLField()
 
-    def getSongInfo(slef):
+    def getSongInfo(self):
+        return "Song name: " + self.song_name
+
+    def getSongUrl(self):
         pass
 
 
-class Album(models.Model):
-    id_album = models.IntegerField(blank=False, null=False)
-    name_album = models.TextField()
+class PlayList(models.Model):
+    id_playlist = models.IntegerField()
+    name_playlist = models.TextField()
+    songs = models.ForeignKey(Song)
     url = models.URLField()
 
-    def getAlbumInfo(slef):
+    def getPlayListInfo(self):
+        return "Play list name: " + self.name_playlist
+
+    def getPlayListUrl(self):
         pass
 
 
+# creo que esta clase no es necesaria
 class User(models.Model):
     id_user = models.IntegerField()
     name_user = models.TextField()
-
-    def getAlbumInfo(slef):
-        pass
+    playlist = models.ForeignKey(PlayList)
