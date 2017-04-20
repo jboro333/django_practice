0576b352376d1f99250e5cdbf2986abc6a0a2402
Alberto Sanmartin Martinez <albertosanmartinmartinez@MacBook-Pro-de-Alberto.local>
2017-04-20 10:23:47 +0200

    more worked

--- django_practice/api_access.py
+++ django_practice/api_access.py
@@ -3,6 +3,10 @@
 # -*- coding: utf-8 -*-
 # vim: set fileencoding = utf8:
 
+import urllib2
+import requests
+from bs4 import BeautifulSoup
+
 
 if __name__ == "__main__":
     pass
--- django_practice/idjango_practice/models.py
+++ django_practice/idjango_practice/models.py
@@ -14,13 +14,14 @@ class Artist(models.Model):
 
     def getArtistInfo(self):
         return "Artist name: " + self.artist_name
-		"""
+	"""
     def __unicode__(self):
         return u"%s" % self.artist_name
 
     def getArtistUrl(self):
         pass
-		"""
+    """
+
 
 class Album(models.Model):
     id_album = models.IntegerField()
@@ -30,14 +31,14 @@ class Album(models.Model):
 
     def getAlbumInfo(self):
         return "Album name: " + self.album_name
-
-		""" 
-		def __unicode__(self):
+    """
+    def __unicode__(self):
         return u"%s" % self.album_name
 
     def getAlbumUrl(self):
         pass
-		"""
+        """
+
 
 class Song(models.Model):
     id_song = models.IntegerField()
@@ -48,31 +49,28 @@ class Song(models.Model):
 
     def getSongInfo(self):
         return "Song name: " + self.song_name
-		"""
+    """
     def __unicode__(self):
         return u"%s" % self.song_name
 
     def getSongUrl(self):
         pass
-  	"""     
+        """
+
+
 class PlayList(models.Model):
     id_playlist = models.IntegerField()
     name_playlist = models.TextField()
     songs = models.ForeignKey(Song)
     url = models.URLField()
-    #playlist = models.ForeignKey(PlayList)
+    # playlist = models.ForeignKey(PlayList)
 
     def getPlayListInfo(self):
         return "Play list name: " + self.name_playlist
-		"""
+    """
     def __unicode__(self):
         return u"%s" % self.name_playlist
 
     def getPlayListUrl(self):
         pass
-
-		"""
-
-
-
-
+        """
