c909c9b74759291008da487ce305095c87c4e386
Alberto Sanmartin Martinez <albertosanmartinmartinez@MacBook-Pro-de-Alberto.local>
2017-04-20 10:39:21 +0200

    work

--- django_practice/idjango_practice/admin.py
+++ django_practice/idjango_practice/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from idjango_practice.models import Artist, Song, Album, User, Playlist
+from idjango_practice.models import Artist, Song, Album, User, PlayList
 
 admin.site.register(Artist)
 admin.site.register(Song)
 admin.site.register(Album)
 admin.site.register(User)
-admin.site.register(Playlist)
+admin.site.register(PlayList)
 # Register your models here.
