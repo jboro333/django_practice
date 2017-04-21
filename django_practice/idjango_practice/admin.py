ae8025b652464a7ea0daf78e3b20d6528b277484
“Jordi” <jbg8@alumnes.udl.cat>
2017-04-21 12:28:46 +0200

    final

--- django_practice/idjango_practice/admin.py
+++ django_practice/idjango_practice/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 # import models
-from idjango_practice.models import Album, Artist, Song, Playlist
+from idjango_practice.models import Album, Artista, Song, Playlist
 
 admin.site.register(Album)
-admin.site.register(Artist)
+admin.site.register(Artista)
 admin.site.register(Song)
 admin.site.register(Playlist)
--- django_practice/idjango_practice/models.py
+++ django_practice/idjango_practice/models.py
@@ -4,26 +4,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Artista(models.Model):
+    id_artista = models.IntegerField(default=0)
+    name_artista = models.TextField(max_length=50)
+    artista = models.ForeignKey(User)
+
+    def __unicode__(self):
+            return self.id_artista
+
+
 class Album(models.Model):
     id_album = models.IntegerField()
     name_album = models.TextField(max_length=50)
+    album = models.ForeignKey(Artista)
 
     def __unicode__(self):
             return self.id_album
 
 
-class Artist(models.Model):
-    id_artist = models.IntegerField()
-    name_artist = models.TextField(max_length=50)
-    # artist = models.ForeignKey(User)
-
-    def __unicode__(self):
-            pass
-
-
 class Song(models.Model):
     id_song = models.IntegerField()
     name_song = models.TextField()
+    song = models.ForeignKey(Album, Artista)
 
     def __unicode__(self):
             return self.id_song
@@ -32,6 +34,7 @@ class Song(models.Model):
 class Playlist(models.Model):
     id_playlist = models.IntegerField()
     name_playlist = models.TextField(max_length=50)
+    playlist = models.ForeignKey(Song)
 
     def __unicode__(self):
-            pass
+            return self.id_playlist
