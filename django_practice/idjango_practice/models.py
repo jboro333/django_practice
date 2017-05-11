0f5ecd1802fa843c85a05dab124852f4a8f81b91
Alberto Sanmartin Martinez <albertosanmartinmartinez@gmail.com>
2017-05-11 19:25:38 +0200

    error solved

--- django_practice/idjango_practice/models.py
+++ django_practice/idjango_practice/models.py
@@ -1,40 +1,52 @@
 # from __future__ import unicode_literals
 from django.db import models
-
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 
 class Artista(models.Model):
-    id_artista = models.IntegerField(default=0)
+    id_artista = models.AutoField(primary_key=True)
     name_artista = models.TextField(max_length=50)
-    artista = models.ForeignKey(User)
+    # user = models.ForeignKey(User)
 
     def __unicode__(self):
-            return self.id_artista
+            return self.name_artista
+
+    def __str__(self):  # python 3
+        return self.name_artista
 
 
 class Album(models.Model):
-    id_album = models.IntegerField()
+    id_album = models.AutoField(primary_key=True)
     name_album = models.TextField(max_length=50)
-    album = models.ForeignKey(Artista)
+    artista = models.ForeignKey(Artista)
 
     def __unicode__(self):
-            return self.id_album
+            return self.name_album
+
+    def __str__(self):  # python 3
+        return self.name_album
 
 
 class Song(models.Model):
-    id_song = models.IntegerField()
-    name_song = models.TextField()
-    song = models.ForeignKey(Album, Artista)
+    id_song = models.AutoField(primary_key=True)
+    name_song = models.TextField(max_length=50)
+    artista = models.ForeignKey(Artista)
+    album = models.ForeignKey(Album)
 
     def __unicode__(self):
-            return self.id_song
+            return self.name_song
+
+    def __str__(self):  # python 3
+        return self.name_song
 
 
 class Playlist(models.Model):
-    id_playlist = models.IntegerField()
+    id_playlist = models.AutoField(primary_key=True)
     name_playlist = models.TextField(max_length=50)
-    playlist = models.ForeignKey(Song)
+    # playlist = models.ForeignKey(Song)
 
     def __unicode__(self):
-            return self.id_playlist
+            return self.name_playlist
+
+    def __str__(self):  # python 3
+        return self.name_playlist
