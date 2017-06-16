ff33bbf7e53097e792f72adeb521852cbc46f1cc
Juan <juanbone16@gmail.com>
2017-06-16 19:38:11 +0200

    Solucionat un error en reviws i canvi urls

--- django_practice/django_practice/urls.py
+++ django_practice/django_practice/urls.py
@@ -36,7 +36,10 @@ from idjango_practice.views import review
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
+    url(r'^home', views.home, name="home"),
     url(r'^login/$', views.Login),
+    url(r'^logout/$', views.Logout),
+    url(r'^contact', views.contact, name="contact"),
     url(r'^musica/', include('idjango_practice.urls', namespace='idjango_practice')),
     url(r'^artists/(?P<pk>\d+)/reviews/create/$',
         review,
