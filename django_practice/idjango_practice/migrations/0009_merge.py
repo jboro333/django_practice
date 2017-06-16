af245831771144e26d4e7569d9d8074dc79e4e0d
Alberto Sanmartin Martinez <albertosanmartinmartinez@gmail.com>
2017-06-16 20:43:12 +0200

    more work

---
+++ django_practice/idjango_practice/migrations/0009_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('idjango_practice', '0008_merge_20170616_1742'),
+        ('idjango_practice', '0008_merge_20170616_1809'),
+        ('idjango_practice', '0008_merge'),
+    ]
+
+    operations = [
+    ]
